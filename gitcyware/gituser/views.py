2c5ee4ce18b30adc6ee775696199b6cfdd9891a3
pupul <pupul.ranjan@ansrsource.com>
2017-09-04 18:18:56 +0530

    append

--- gitcyware/gituser/views.py
+++ gitcyware/gituser/views.py
@@ -13,28 +13,18 @@ import ast
 # Create your views here.
 class ShowResult(APIView):
     def get(self, request):
-        # import ipdb; ipdb.set_trace()
-        searchresult = []
         try:
             try:
+                jljljl= []
+                searchresult={}
                 searchresult = requests.get('https://api.github.com/users')
-                print searchresult
                 for val in searchresult:
-                    print type((val))
+                    jljljl.append(val)
+
+                return HttpResponse(jljljl, content_type="application/json")
 
             except Exception as e:
                 print e
-
-
-            if name:
-                searchvalue = EmployeeDetails.objects.filter(employeeRole__icontains=name).values('company__companyName', 'jobDetail__roleName', 'employeeName')
-                if searchvalue:
-                    serachresult = list(searchvalue)
-                    serachresult = json.dumps(serachresult)
-                    return HttpResponse(serachresult, content_type="application/json")
-                else:
-                    return HttpResponse('NO result Matching Your Query...', content_type="application/json")
-            else:
-                return HttpResponse('NO Result Found...', content_type="application/json")
+                return HttpResponse('nothing...', content_type="application/json")
         except Exception as e:
             return HttpResponse('search Something...', content_type="application/json")
