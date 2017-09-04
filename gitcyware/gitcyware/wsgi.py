85c97ff059d6d302e2b56bc7359e29e2df7f7e40
pupul <pupul.ranjan@ansrsource.com>
2017-09-04 12:32:16 +0530

    initial commit

---
+++ gitcyware/gitcyware/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gitcyware project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gitcyware.settings")
+
+application = get_wsgi_application()
